2d7d116631b8ff50d10674fdc00a628bc292c73c
tomlum <checkeredlines@gmail.com>
2017-10-26 00:35:02 -0400

    pipenv and general reorganizing

--- README.md
+++ README.md
@@ -4,13 +4,9 @@
 
 A proof of concept for Deep Sleep Walk, CountSheep learns to look at an mnist digit and draw the next mnist digit.  Of course, in the process of trying to draw an image from just neural memories, CountSheep distorts its reinterpritation in sometimes chaotic and sometimes meaningful ways.
 
-To begin counting sheep you'll need:
-
-* Python 3
-* keras
-* matplotlib
-* glob
-* numpy
+# CountSheep
+To begin counting sheep you'll just need to run
+`pipenv install`
 
 Which I recommend you install with pip!
 
--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import numpy as np
-
-# For debugging without pulling all of Keras
-loadKeras = True
-
-if loadKeras:
-    from keras.models import Sequential
-    from keras.layers import Dense, Activation
-    from keras.layers import Dropout
-    from keras.utils import np_utils
-
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 import matplotlib
-matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 import matplotlib.image as mpim
 import glob
+matplotlib.use("TKAgg")
 
 # Model Parameters
-epochs = 1000
+epochs = 500
 
 # Import the Data
 batch_in = []
@@ -37,29 +29,30 @@ batch_out = batch_out / 255
 im_test = batch_in[1]
 
 # The Model
-if loadKeras:
-    model = Sequential([
-        Dense(32, input_shape=(784,)),
-        Activation("relu"),
-        Dense(784),
-        Activation("relu")
-    ])
+model = Sequential([
+    Dense(32, input_shape=(784,)),
+    Activation("relu"),
+    Dense(784),
+    Activation("relu")
+])
 
-    model.compile(loss="mean_squared_error",
-                  optimizer="adam", metrics=["accuracy"])
+model.compile(loss="mean_squared_error",
+              optimizer="adam", metrics=["accuracy"])
 
-    model.fit(batch_in, batch_out, epochs=epochs, batch_size=10, verbose=2)
-    scores = model.evaluate(batch_in, batch_out, verbose=0)
+model.fit(batch_in, batch_out, epochs=epochs, batch_size=10, verbose=2)
+scores = model.evaluate(batch_in, batch_out, verbose=0)
 
-#First Seed/Base Prediction
+# First Seed/Base Prediction
 prediction = im_test.reshape(28, 28)
 
 # Interact with the NN
+
+
 def interact(event):
 
     global prediction
 
-    if event.key == 'right':
+    if event.key == "right":
         # Generate/Predict the next number
         prediction = model.predict(
             prediction.reshape(-1, 784)).reshape(28, 28)
@@ -78,8 +71,9 @@ def interact(event):
                 fig.canvas.draw()
                 break
 
+
 # Setup the canvas
 fig, ax = plt.subplots()
-fig.canvas.mpl_connect('key_press_event', interact)
+fig.canvas.mpl_connect("key_press_event", interact)
 plt.imshow(prediction)
 plt.show()
